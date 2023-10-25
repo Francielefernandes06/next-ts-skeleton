28c31e0723aa4e504da24d174773e65113deb048
Cristiano <cristiano-junior10@outlook.com>
2023-10-25 10:59:32 -0300

    first commit

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -7,8 +7,6 @@ const About = () => {
       <Head>
           <title>About page</title>
           <meta name="description" content="About page content description" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
       </Head>
       <h2>About page</h2>
       <Link href="/">Home Page</Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,6 @@ const Home = () => {
       <Head>
           <title>Home page</title>
           <meta name="description" content="Home page content description" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
       </Head>
       <h2>Home page</h2>
       <Link href="/about">
