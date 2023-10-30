8029c9397e0a6605352e7b10db653dc41635eba6
franciele <francielefernandes126@gmail.com>
2023-10-30 13:09:58 -0300

    style: ajuste no header

--- layouts/Header/index.tsx
+++ layouts/Header/index.tsx
@@ -22,10 +22,16 @@ function Header({ show, handleClose }) {
           <Offcanvas.Title></Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-        <Form.Control
-        type="string"
-        placeholder="Procure por leilão, lote, raça ou código" 
-      />
+        <div className="input-group p-2">
+              <Form.Control
+                type="string"
+                className="border-end-0 p-3"
+                placeholder="Procure por leilão, lote, raça ou código"
+              />
+              <span className="input-group-text bg-white border-end-0 border-start-0">
+                <img src={LOGO_SEARCH} alt="Search Icon" />
+              </span>
+            </div>
 
 <ListGroup  className={styles.links}  variant="flush">
       <ListGroup.Item className={styles.link} action href="#link1">Home <i className="bi bi-chevron-right"></i>
